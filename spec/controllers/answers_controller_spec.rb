d023945c9261d63a1b942bd94da061bdeb1a9303
peresvetjke <igor.udalov@gmail.com>
2021-11-12 07:50:49 +0300

    Add specs in controllers for authorization validation on creating questions and answers

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,26 +5,42 @@ RSpec.describe AnswersController, :type => :controller do
   describe "POST create" do
     let (:question) { create(:question) }
 
-    context 'with valid params' do
-      it "creates new answer in db" do
-        expect { post :create, params: { answer: attributes_for(:answer), question_id: question }}.to change(question.answers, :count).by(1)
-      end
-
-      it "renders show question template with new answer created" do
+    context "when unauthorized" do
+      it "renders log_in template" do
         post :create, params: { answer: attributes_for(:answer), question_id: question }
-        expect(response).to redirect_to(question)
-      end
+        expect(response).to redirect_to(new_user_session_path)
+      end 
     end
 
-    context 'with invalid params' do
-      it "keeps count unchanged" do
-        expect { post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question } }.to change(question.answers, :count).by(0)
+    context "when authorized" do
+      before {
+        user = create(:user)
+        login(user)
+      }
+
+      context 'with valid params' do
+        it "creates new answer in db" do
+          expect { post :create, params: { answer: attributes_for(:answer), question_id: question }}.to change(question.answers, :count).by(1)
+        end
+        
+        it "renders show question template with new answer created" do
+          post :create, params: { answer: attributes_for(:answer), question_id: question }
+          expect(response).to redirect_to(question)
+        end
       end
 
-      it "renders show question template with answer unsaved" do
-        post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question }
-        expect(response).to render_template "questions/show"
+      context 'with invalid params' do
+        it "keeps count unchanged" do
+          expect { post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question } }.to change(question.answers, :count).by(0)
+        end
+
+        it "renders show question template with answer unsaved" do
+          post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question }
+          expect(response).to render_template "questions/show"
+        end
       end
+
     end
+
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -2,10 +2,6 @@ require "rails_helper"
 
 RSpec.describe QuestionsController, :type => :controller do
 
-  before {
-    user = create(:user)
-    login(user)
-  }
 
   describe "GET show" do
     let (:question) { create(:question) }
@@ -17,31 +13,59 @@ RSpec.describe QuestionsController, :type => :controller do
   end
 
   describe "GET new" do
-    it "renders new template" do
-      get :new
-      expect(response).to render_template(:new)
+    context "when unauthorized" do
+      it "renders log_in template" do
+        get :new
+        expect(response).to redirect_to(new_user_session_path)
+      end      
+    end
+
+    context "when authorized" do
+      before {
+        user = create(:user)
+        login(user)
+      }
+
+      it "renders new template" do
+        get :new
+        expect(response).to render_template(:new)
+      end
     end
   end
 
   describe "POST create" do    
-    context 'with valid params' do
-      it "creates new question in db" do
-        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
-      end
-
-      it "renders show template with object created" do 
+    context "when unauthorized" do
+      it "renders log_in template" do 
         post :create, params: { question: attributes_for(:question) }
-        expect(response).to redirect_to(controller.question)
+        expect(response).to redirect_to(new_user_session_path)
       end
     end
 
-    context 'with invalid params' do
-      it "keeps count unchanged" do
-        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to change(Question, :count).by(0)
+    context "when authorized" do
+      before {
+        user = create(:user)
+        login(user)
+      }
+
+      context 'with invalid params' do
+        it "keeps count unchanged" do
+          expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to change(Question, :count).by(0)
+        end
+        it "renders new template" do
+          post :create, params: { question: attributes_for(:question, :invalid) }
+          expect(response).to render_template :new
+        end
       end
-      it "renders new template" do
-        post :create, params: { question: attributes_for(:question, :invalid) }
-        expect(response).to render_template :new
+
+      context 'with valid params' do
+        it "creates new question in db" do
+          expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+        end
+
+        it "renders show template with object created" do 
+          post :create, params: { question: attributes_for(:question) }
+          expect(response).to redirect_to(controller.question)
+        end
       end
     end
   end
