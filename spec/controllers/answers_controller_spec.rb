184d35644f0d5db72d33ef0fd542677ee4d77f8f
peresvetjke <igor.udalov@gmail.com>
2021-11-16 09:22:05 +0300

    Update POST create of answers_controller spec

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -8,9 +8,9 @@ RSpec.describe AnswersController, :type => :controller do
 
   describe "POST create" do
     context "when unauthorized" do
-      it "renders log_in template" do
-        post :create, params: { answer: attributes_for(:answer), question_id: question }
-        expect(response).to redirect_to(new_user_session_path)
+      it "returns 401 unauthorized status" do
+        post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js} 
+        expect(response).to have_http_status(401)
       end 
     end
 
@@ -19,21 +19,21 @@ RSpec.describe AnswersController, :type => :controller do
 
       context 'with invalid params' do
         it "keeps count unchanged" do
-          expect { post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question } }.to change(question.answers, :count).by(0)
+          expect { post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question, format: :js } }.to change(question.answers, :count).by(0)
         end
-        it "renders show question template with answer unsaved" do
-          post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question }
-          expect(response).to render_template "questions/show"
+        it "renders create" do
+          post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question, format: :js }
+          expect(response).to render_template :create
         end
       end
 
       context 'with valid params' do
         it "creates new answer in db" do
-          expect { post :create, params: { answer: attributes_for(:answer), question_id: question, author_id: user }}.to change(question.answers, :count).by(1)
+          expect { post :create, params: { answer: attributes_for(:answer), question_id: question, author_id: user }, format: :js}.to change(question.answers, :count).by(1)
         end       
-        it "renders show question template with new answer created" do
-          post :create, params: { answer: attributes_for(:answer), question_id: question }
-          expect(response).to redirect_to(question)
+        it "renders create" do
+          post :create, params: { answer: attributes_for(:answer), question_id: question, format: :js }
+          expect(response).to render_template :create
         end
       end
     end
