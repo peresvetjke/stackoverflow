4911ff2c918e3fc8de11382a4af08b5c7d7207bb
peresvetjke <igor.udalov@gmail.com>
2021-12-07 09:06:11 +0300

    Refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
 
   resources :questions, shallow: true do
     post :accept_vote, to: "votes#accept", on: :member, defaults: { votable: 'questions' }
-    resources :comments, defaults: { commentable: 'questions' }
+    resources :comments, defaults: { commentable: 'questions' }, only: %i[create update destroy]
 
     resources :answers, shallow: true, only: %i[create edit update destroy] do
       post :accept_vote, to: "votes#accept", on: :member, defaults: { votable: 'answers' }
-      resources :comments, defaults: { commentable: 'answers' }
+      resources :comments, defaults: { commentable: 'answers' }, only: %i[create update destroy]
     
       post :mark_best, on: :member
     end
