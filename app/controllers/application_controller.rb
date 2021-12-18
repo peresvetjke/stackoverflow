d01e9b0a2af15a9ac2deb36f6e8422e481600836
peresvetjke <igor.udalov@gmail.com>
2021-12-18 08:10:03 +0300

    Add corrections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,11 @@ class ApplicationController < ActionController::Base
   check_authorization unless: :devise_controller?
 
   rescue_from CanCan::AccessDenied do |exception|
-    if current_user.nil?
-      session[:next] = request.fullpath
-      redirect_to login_url, alert: 'You have to log in to continue.'
-    else
-      exception.default_message = "Not authorized"
-      respond_to do |format|
-        format.json { render json: exception.message, status: :forbidden }
-        format.js   { render js: exception.message, status: :forbidden }
-        format.html { redirect_to main_app.root_url, alert: exception.message }
-      end
+    exception.default_message = "Not authorized"
+    respond_to do |format|
+      format.json { render json: exception.message, status: :forbidden }
+      format.js   { render js: exception.message, status: :forbidden }
+      format.html { redirect_to main_app.root_url, alert: exception.message }
     end
   end
 
