160f9a896498bfe440cb796e912ba451e343e45f
peresvetjke <igor.udalov@gmail.com>
2021-12-19 07:49:14 +0300

    Add helpers

--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -3,26 +3,26 @@ require "rails_helper"
 describe "Profiles API", type: :request do
   let(:headers) { { "CONTENT_TYPE"  => "application/json",
                     "ACCEPT"        => "application/json" } }
+  let(:me)           { create(:user) }
+  let(:access_token) { create(:access_token, resource_owner_id: me.id) }
 
   describe "GET /api/v1/profiles/me" do
+    let(:path) { "/api/v1/profiles/me" }
+    
     context "when unauthorized" do
       it "returns 401 status if there is no access_token" do
-        get "/api/v1/profiles/me", headers: headers
+        do_request("get", path, headers: headers)
         expect(response.status).to eq 401
       end
 
       it "returns 401 status if there is invalid" do
-        get "/api/v1/profiles/me", params: { access_token: "1234" }, headers: headers
+        do_request("get", path, params: { access_token: "1234" }, headers: headers)
         expect(response.status).to eq 401
       end
     end
 
     context "when authorized" do
-      let(:me)           { create(:user) }
-      let(:access_token) { create(:access_token, resource_owner_id: me.id) }
-      let(:json)         { JSON.parse(response.body) }
-
-      before { get "/api/v1/profiles/me", params: { access_token: access_token.token }, headers: headers }
+      before { do_request("get", path, params: { access_token: access_token.token }, headers: headers) }
 
       it "returns 200 status" do
         expect(response.status).to eq 200
@@ -41,9 +41,8 @@ describe "Profiles API", type: :request do
       end
     end    
   end
+
+  describe "GET /api/v1/profiles" do
+
+  end
 end
-
-
-
-
-
